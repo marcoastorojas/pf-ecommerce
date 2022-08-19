29a9ba4a65833862a032eced9c9df44de3f325d6
alejandrogh7 <alejo1garciasosa@gmail.com>
2022-08-19 08:52:16 -0500

    fix-shopping-buttons-details

--- front/app/src/App.js
+++ front/app/src/App.js
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" exact element={<Landing />} />
-        <Route path="/product/details/:id" exact element={<Details />} />
+        <Route path="/product/:id" exact element={<Details />} />
       </Routes>
     </div>
   );
