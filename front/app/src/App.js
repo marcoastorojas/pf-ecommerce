9a6aac3f7a33cdd852b17d505fa5a9c769d185b1
alejandrogh7 <alejo1garciasosa@gmail.com>
2022-08-18 19:10:28 -0500

    details

--- front/app/src/App.js
+++ front/app/src/App.js
@@ -1,17 +1,20 @@
 // import logo from './logo.svg';
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import Landing from "./pages/Landing";
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+import Landing from "./pages/Landing";
+import Details from "./pages/Details/Details";
 
 function App() {
- return (
-  <div className="App">
-   <Routes>
-    <Route path="/" exact element={<Landing />} />
-   </Routes>
-  </div>
- );
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" exact element={<Landing />} />
+        <Route path="/product/details/:id" exact element={<Details />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
