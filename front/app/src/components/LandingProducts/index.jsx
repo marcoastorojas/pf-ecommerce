fef6421ea3f965682c6bbcbf94d3ba587efc462f
Andres Torres <andrestorres9700@gmail.com>
2022-08-20 09:50:47 -0300

    unified css

--- front/app/src/components/LandingProducts/index.jsx
+++ front/app/src/components/LandingProducts/index.jsx
@@ -2,32 +2,32 @@ import { getProducts } from "../../redux/actions";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import ProductCard from "../ProductCard";
-import "../../Css/LandingProducts.css"
+import "./index.modules.css";
 
 export default function LandingProducts() {
+ const dispatch = useDispatch();
+ const products = useSelector((state) => state.allProducts);
+ const sliceArrayProduct = products.data?.slice(0, 6);
 
-    const dispatch = useDispatch();
-    const products = useSelector((state) => state.allProducts)
-    const sliceArrayProduct = products.data?.slice(0, 6);
+ useEffect(() => {
+  dispatch(getProducts());
+ }, [dispatch]);
 
-    useEffect(() => {
-        dispatch(getProducts());
-      }, [dispatch]);
-
-    return (
-       <main>
-           <div className="container-lProducts">
-               {Array.isArray(sliceArrayProduct) ? sliceArrayProduct.map((product) => {
-                return (
-                    <ProductCard
-                    key={product.id} 
-                    title={product.title}
-                    image={product.images}
-                    />
-                )
-               }) : null}
-           </div>
-       </main>
-    );
-   }
-   
+ return (
+  <main>
+   <div className="container-lProducts">
+    {Array.isArray(sliceArrayProduct)
+     ? sliceArrayProduct.map((product) => {
+        return (
+         <ProductCard
+          key={product.id}
+          title={product.title}
+          image={product.images}
+         />
+        );
+       })
+     : null}
+   </div>
+  </main>
+ );
+}
