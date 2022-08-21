e0c560fc11483da8e47d68720531ea033c3f2160
alejandrogh7 <alejo1garciasosa@gmail.com>
2022-08-21 15:20:48 -0500

    form

--- front/app/src/components/ProductForm/ProductForm.jsx
+++ front/app/src/components/ProductForm/ProductForm.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { postProduct } from "../../redux/actions";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getSubCategories, postProduct } from "../../redux/actions";
 import { validate } from "../../validations/validator";
 import "./ProductForm.css";
 
@@ -12,23 +12,22 @@ export default function ProductForm() {
     images: "",
     description: "",
     price: 0,
+    subcategoryId: "",
   });
   const [errors, setErrors] = useState({});
 
-
   const dispatch = useDispatch();
+  const subCategories = useSelector((state) => state.subCategories);
+
+  useEffect(() => {
+    dispatch(getSubCategories());
+  }, [dispatch]);
 
   const handleChange = (e) => {
-    if (e.target.files) {
-      setInputs({
-        ...inputs,
-        [e.target.name]: e.target.files[0],
-      });
-    } else
-      setInputs({
-        ...inputs,
-        [e.target.name]: e.target.value,
-      });
+    setInputs({
+      ...inputs,
+      [e.target.name]: e.target.value,
+    });
 
     setErrors(validate({ ...inputs, [e.target.name]: e.target.value }));
   };
@@ -43,85 +42,107 @@ export default function ProductForm() {
       images: "",
       description: "",
       price: 0,
+      subcategoryId: "",
     });
   };
 
-  return (
-    <div>
-      <h1>Add product to sell</h1>
-      <form onSubmit={(e) => handleSubmit(e)} className="form">
-        <div>
-          <label>Product name:</label>
-          <input
-            type="text"
-            name="title"
-            value={inputs.title}
-            placeholder="Product name"
-            onChange={(e) => handleChange(e)}
-          />
-          {errors.title && <p className="danger">{errors.title}</p>}
-        </div>
-        <div>
-          <label>Product model:</label>
-          <input
-            type="text"
-            name="model"
-            value={inputs.model}
-            placeholder="Product model"
-            onChange={(e) => handleChange(e)}
-          />
-          {errors.model && <p className="danger">{errors.model}</p>}
-        </div>
-        <div>
-          <label>Product brand:</label>
-          <input
-            type="text"
-            name="brand"
-            value={inputs.brand}
-            placeholder="Product brand"
-            onChange={(e) => handleChange(e)}
-          />
-          {errors.brand && <p className="danger">{errors.brand}</p>}
-        </div>
-        <div>
-          <label>Product description:</label>
-          <input
-            type="text"
-            name="description"
-            value={inputs.description}
-            placeholder="Product description"
-            onChange={(e) => handleChange(e)}
-          />
-          {errors.description && <p className="danger">{errors.description}</p>}
-        </div>
-        <div>
-          <label>Product price:</label>
-          <input
-            type="text"
-            name="price"
-            value={inputs.price}
-            placeholder="Product price"
-            onChange={(e) => handleChange(e)}
-          />
-          {errors.price && <p className="danger">{errors.price}</p>}
-        </div>
-        <div>
+
+  if (subCategories.length > 0)
+    return (
+      <div>
+        <h1>Add product to sell</h1>
+        <form onSubmit={(e) => handleSubmit(e)} className="form">
           <div>
-            <label>Image(s):</label>
+            <label>Product name:</label>
             <input
-              type="file"
-              name="images"
+              type="text"
+              name="title"
+              value={inputs.title}
+              placeholder="Product name"
               onChange={(e) => handleChange(e)}
             />
-            {errors.images && <p className="danger">{errors.images}</p>}
+            {errors.title && <p className="danger">{errors.title}</p>}
           </div>
-          <input
-            type="submit"
-            value="Submit"
-            disabled={!(Object.entries(errors).length === 0)}
-          />
-        </div>
-      </form>
-    </div>
-  );
+          <div>
+            <label>Product model:</label>
+            <input
+              type="text"
+              name="model"
+              value={inputs.model}
+              placeholder="Product model"
+              onChange={(e) => handleChange(e)}
+            />
+            {errors.model && <p className="danger">{errors.model}</p>}
+          </div>
+          <div>
+            <label>Product brand:</label>
+            <input
+              type="text"
+              name="brand"
+              value={inputs.brand}
+              placeholder="Product brand"
+              onChange={(e) => handleChange(e)}
+            />
+            {errors.brand && <p className="danger">{errors.brand}</p>}
+          </div>
+          <div>
+            <label>Product description:</label>
+            <input
+              type="text"
+              name="description"
+              value={inputs.description}
+              placeholder="Product description"
+              onChange={(e) => handleChange(e)}
+            />
+            {errors.description && (
+              <p className="danger">{errors.description}</p>
+            )}
+          </div>
+          <div>
+            <label>Product price:</label>
+            <input
+              type="text"
+              name="price"
+              value={inputs.price}
+              placeholder="Product price"
+              onChange={(e) => handleChange(e)}
+            />
+            {errors.price && <p className="danger">{errors.price}</p>}
+          </div>
+          <div>
+            <div>
+              <label>Image:</label>
+              <input
+                type="text"
+                name="images"
+                placeholder="Product image"
+                onChange={(e) => handleChange(e)}
+              />
+              {errors.images && <p className="danger">{errors.images}</p>}
+            </div>
+            <div>
+              <label>Categories:</label>
+              <select name="subcategoryId" onChange={(e) => handleChange(e)}>
+                <option>CATEGORIES</option>
+                {subCategories &&
+                  subCategories.map((sc) => {
+                    return (
+                      <option value={sc.id} key={sc.id}>
+                        {sc.name}
+                      </option>
+                    );
+                  })}
+              </select>
+            </div>
+            <input
+              type="submit"
+              value="Submit"
+              disabled={!(Object.entries(errors).length === 0)}
+            />
+          </div>
+        </form>
+      </div>
+    );
+
+  return <div>Wating fetching</div>;
 }
--- front/app/src/redux/reducer.js
+++ front/app/src/redux/reducer.js
@@ -1,63 +1,77 @@
 import {
- GET_PRODUCTS,
- GET_PRODUCTS_BY_NAME,
- GET_PRODUCT_BY_ID,
- CLEAR_DETAIL,
- GET_PRODUCTS_FILTER,
- GET_CATEGORIES,
- GET_CATEGORY_PRODUCTS_BY_ID,
+  GET_PRODUCTS,
+  GET_PRODUCTS_BY_NAME,
+  GET_PRODUCT_BY_ID,
+  CLEAR_DETAIL,
+  GET_PRODUCTS_FILTER,
+  GET_CATEGORIES,
+  GET_CATEGORY_PRODUCTS_BY_ID,
+  GET_SUB_CATEGORIES,
 } from "./actions";
 
 const initialState = {
- products: [],
- allProducts: [],
- searchedProducts: [],
- product: {},
- categories: [],
+  products: [],
+  allProducts: [],
+  searchedProducts: [],
+  product: {},
+  categories: [],
+  subCategories: [],
 };
 
 export const reducer = (state = initialState, action) => {
- switch (action.type) {
-  case GET_PRODUCTS:
-   return {
-    ...state,
-    products: action.payload,
-    allProducts: action.payload,
-   };
-  case GET_PRODUCTS_BY_NAME:
-   return {
-    ...state,
-    searchedProducts: [...action.payload],
-   };
-  case GET_PRODUCT_BY_ID: {
-   return {
-    ...state,
-    product: action.payload,
-   };
+  switch (action.type) {
+    case GET_PRODUCTS:
+      return {
+        ...state,
+        products: action.payload,
+        allProducts: action.payload,
+      };
+    case GET_PRODUCTS_BY_NAME:
+      return {
+        ...state,
+        searchedProducts: [...action.payload],
+      };
+    case GET_PRODUCT_BY_ID: {
+      return {
+        ...state,
+        product: action.payload,
+      };
+    }
+    case CLEAR_DETAIL: {
+      return {
+        ...state,
+        product: {},
+      };
+    }
+    case GET_PRODUCTS_FILTER: {
+      return {
+        ...state,
+        searchedProducts: [...action.payload],
+      };
+    }
+    case GET_CATEGORIES:
+      return {
+        ...state,
+        categories: [...action.payload],
+      };
+    case GET_CATEGORY_PRODUCTS_BY_ID:
+      return {
+        ...state,
+        searchedProducts: [...action.payload],
+      };
+    case GET_SUB_CATEGORIES: {
+      const info = action.payload.data.map((sc) => {
+        return {
+          name: sc.name,
+          id: sc.id,
+        };
+      });
+      return {
+        ...state,
+        subCategories: info,
+      };
+    }
+    default:
+      return state;
   }
-  case CLEAR_DETAIL: {
-   return {
-    ...state,
-    product: {},
-   };
-  }
-  case GET_PRODUCTS_FILTER: {
-   return {
-    ...state,
-    searchedProducts: [...action.payload],
-   };
-  }
-  case GET_CATEGORIES:
-   return {
-    ...state,
-    categories: [...action.payload],
-   };
-  case GET_CATEGORY_PRODUCTS_BY_ID:
-   return {
-    ...state,
-    searchedProducts: [...action.payload],
-   };
-  default:
-   return state;
- }
 };
