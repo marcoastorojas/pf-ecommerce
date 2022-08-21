559182af8080ae1639ab4022bf3a009226429a28
Marco Asto <marcoasto123@gmail.com>
2022-08-21 14:20:30 -0500

    morgan added

--- back/src/app.js
+++ back/src/app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const { routes } = require("./routes")
 const cors = require("cors")
+const morgan = require('morgan');
+
+
 const server = express()
 
+server.use(morgan('dev'));
 server.use(express.json())
 server.use(cors())
 server.use("/",routes)
+
 module.exports = server
