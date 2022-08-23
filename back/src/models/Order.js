2c82d596cea00a98c6b7206ea36e7831841658d9
ElRuidoyLaFuria <97857484+ElRuidoyLaFuria@users.noreply.github.com>
2022-08-23 17:20:13 -0300

    field name correct.

--- back/src/models/Order.js
+++ back/src/models/Order.js
@@ -10,7 +10,7 @@ module.exports = (sequelize) => {
             allowNull: false,
             primaryKey: true,
         },
-        fecha: {
+        date: {
             type: DataTypes.STRING,
             allowNull: false,
         }
