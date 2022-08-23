e0d95199c0a783c55a73cb8861c0fb09f1e24a04
Andres Torres <andrestorres9700@gmail.com>
2022-08-23 20:35:57 -0300

    deploy test

--- front/app/src/redux/actions.js
+++ front/app/src/redux/actions.js
@@ -20,52 +20,46 @@ export const SET_USER_GOOGLE = "SET_USER_GOOGLE";
 
 const BASE_URL = `http://localhost:3001`;
 
-const BASE_URL = 'http://localhost:3001'
-
 export const getProducts = () => {
-  return async function (dispatch) {
-   try {
-    let json = await axios.get(`${BASE_URL}/products`);
-    return dispatch({
-     type: GET_PRODUCTS,
-     payload: json.data,
-    });
-   } catch (error) {
-    console.log(error);
-   }
-  };
+ return async function (dispatch) {
+  try {
+   let json = await axios.get(`${BASE_URL}/products`);
+   return dispatch({
+    type: GET_PRODUCTS,
+    payload: json.data,
+   });
+  } catch (error) {
+   console.log(error);
+  }
  };
 };
 
 export const getProductId = (id) => {
-  return async function (dispatch) {
-    try {
-      const response = await axios.get(`${BASE_URL}/products/${id}`);
-      return dispatch({
-        type: GET_PRODUCT_BY_ID,
-        payload: response.data,
-      });
-    } catch (error) {
-      console.log(`can not find product with id: ${id}`, error);
-    }
-  };
+ return async function (dispatch) {
+  try {
+   const response = await axios.get(`${BASE_URL}/products/${id}`);
+   return dispatch({
+    type: GET_PRODUCT_BY_ID,
+    payload: response.data,
+   });
+  } catch (error) {
+   console.log(`can not find product with id: ${id}`, error);
+  }
+ };
 };
 
 export const postProduct = (product) => {
-  return async function (dispatch) {
-    try {
-      const response = await axios.post(
-        `${BASE_URL}/products`,
-        product
-      );
-      return dispatch({
-        type: POST_PRODUCT,
-        payload: response.data,
-      });
-    } catch (error) {
-      console.log("can not post product", error);
-    }
-  };
+ return async function (dispatch) {
+  try {
+   const response = await axios.post(`${BASE_URL}/products`, product);
+   return dispatch({
+    type: POST_PRODUCT,
+    payload: response.data,
+   });
+  } catch (error) {
+   console.log("can not post product", error);
+  }
+ };
 };
 
 export const clearDetail = () => {
@@ -75,33 +69,32 @@ export const clearDetail = () => {
 };
 
 export const getProductsByName = (textInput) => {
-
-  return (dispatch) => {
-    axios
-      .get(`${BASE_URL}/products?name=${textInput}`)
-      .then((response) => {
-        console.log({
-          from: "action creator getProductsById",
-          response: response,
-        });
-        dispatch({
-          type: GET_PRODUCTS_BY_NAME,
-          payload: response.data.data,
-        });
-      })
-      .catch((err) => {
-        console.log({ from: "action creator getProductsByName", err });
-      });
-  };
+ return (dispatch) => {
+  axios
+   .get(`${BASE_URL}/products?name=${textInput}`)
+   .then((response) => {
+    console.log({
+     from: "action creator getProductsById",
+     response: response,
+    });
+    dispatch({
+     type: GET_PRODUCTS_BY_NAME,
+     payload: response.data.data,
+    });
+   })
+   .catch((err) => {
+    console.log({ from: "action creator getProductsByName", err });
+   });
+ };
 };
 
 export const getProductsFilter = (name, max, min, asc, desc) => {
-  let url = new URL(`${BASE_URL}/products`);
-  if (!!name) url.searchParams.append("name", name);
-  if (!!max) url.searchParams.append("max", max);
-  if (!!min) url.searchParams.append("min", min);
-  if (!!asc) url.searchParams.append("asc", asc);
-  if (!!desc) url.searchParams.append("desc", desc);
+ let url = new URL(`${BASE_URL}/products`);
+ if (!!name) url.searchParams.append("name", name);
+ if (!!max) url.searchParams.append("max", max);
+ if (!!min) url.searchParams.append("min", min);
+ if (!!asc) url.searchParams.append("asc", asc);
+ if (!!desc) url.searchParams.append("desc", desc);
  //  console.log(url.href);
  return (dispatch) => {
   axios
@@ -132,14 +125,20 @@ export const getCategories = () => {
  };
 };
 
-
-export const getCategoryProductsById = (categoryId, name, max, min, asc, desc) => {
-  let url = new URL(`${BASE_URL}/products/category/${categoryId}`);
-  if (!!name) url.searchParams.append("name", name);
-  if (!!max) url.searchParams.append("max", max);
-  if (!!min) url.searchParams.append("min", min);
-  if (!!asc) url.searchParams.append("asc", asc);
-  if (!!desc) url.searchParams.append("desc", desc);
+export const getCategoryProductsById = (
+ categoryId,
+ name,
+ max,
+ min,
+ asc,
+ desc
+) => {
+ let url = new URL(`${BASE_URL}/products/category/${categoryId}`);
+ if (!!name) url.searchParams.append("name", name);
+ if (!!max) url.searchParams.append("max", max);
+ if (!!min) url.searchParams.append("min", min);
+ if (!!asc) url.searchParams.append("asc", asc);
+ if (!!desc) url.searchParams.append("desc", desc);
  return (dispatch) => {
   axios
    .get(url.href)
@@ -164,17 +163,17 @@ export const getSearchName = (payload) => {
 };
 
 export const getSubCategories = () => {
-  return async function (dispatch) {
-    try {
-      const response = await axios.get(`${BASE_URL}/subCategories`);
-      return dispatch({
-        type: GET_SUB_CATEGORIES,
-        payload: response.data,
-      });
-    } catch (error) {
-      console.log(`can not find subcategories`, error);
-    }
-  };
+ return async function (dispatch) {
+  try {
+   const response = await axios.get(`${BASE_URL}/subCategories`);
+   return dispatch({
+    type: GET_SUB_CATEGORIES,
+    payload: response.data,
+   });
+  } catch (error) {
+   console.log(`can not find subcategories`, error);
+  }
+ };
 };
 
 export const getSearchCategory = (payload) => {
