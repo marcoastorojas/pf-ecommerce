142dea0ff62c658464927e851202953b52973b04
alejandrogh7 <alejo1garciasosa@gmail.com>
2022-08-24 14:18:20 -0500

    shopping-cart

--- front/app/src/components/NavBar/index.jsx
+++ front/app/src/components/NavBar/index.jsx
@@ -4,55 +4,56 @@ import { useDispatch, useSelector } from "react-redux";
 import SignInGoogle from "../SignInGoogle/SigInGoogle.jsx";
 
 import {
- getCategories,
- getCategoryProductsById,
- getSearchCategory,
- getSearchName,
+  getCategories,
+  getCategoryProductsById,
+  getSearchCategory,
+  getSearchName,
 } from "../../redux/actions";
 
 import SearchBar from "../SearchBar";
 
 import style from "./index.module.css";
 
-import cart from "../../media/images/cart.svg";
+import cartI from "../../media/images/cart.svg";
 
 export default function NavBar() {
- const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
- const categories = useSelector((state) => state.categories);
+  const categories = useSelector((state) => state.categories);
+  const cart = useSelector((state) => state.cart);
 
- useEffect(() => {
-  dispatch(getCategories());
- }, [dispatch]);
+  useEffect(() => {
+    dispatch(getCategories());
+  }, [dispatch]);
 
- //  const [productNumber, setProductNumber] = useState(0);
- const [showCategories, setShowCategories] = useState(false);
+  //  const [productNumber, setProductNumber] = useState(0);
+  const [showCategories, setShowCategories] = useState(false);
 
- const onCategorySelection = (e) => {
-  // console.log(e);
-  dispatch(getCategoryProductsById(e.target.id));
-  dispatch(getSearchCategory(e.target.id));
-  dispatch(getSearchName(""));
- };
+  const onCategorySelection = (e) => {
+    // console.log(e);
+    dispatch(getCategoryProductsById(e.target.id));
+    dispatch(getSearchCategory(e.target.id));
+    dispatch(getSearchName(""));
+  };
 
- function showCategoriesHandler() {
-  showCategories ? setShowCategories(false) : setShowCategories(true);
- }
+  function showCategoriesHandler() {
+    showCategories ? setShowCategories(false) : setShowCategories(true);
+  }
 
- return (
-  <header className={style.header}>
-   <div className={style.sectionOne}>
-    <div className={style.logoAndSB}>
-     {" "}
-     <Link to={"/"} className={style.logoLink}>
-      <p className={style.logo}>PF: e-commerce</p>
-     </Link>
-     <div className={style.searchBarDiv}>
-      <SearchBar />
-     </div>
-    </div>
-    <nav className={style.navButtons}>
-     {/* <details id="categories" className={style.details}>
+  return (
+    <header className={style.header}>
+      <div className={style.sectionOne}>
+        <div className={style.logoAndSB}>
+          {" "}
+          <Link to={"/"} className={style.logoLink}>
+            <p className={style.logo}>PF: e-commerce</p>
+          </Link>
+          <div className={style.searchBarDiv}>
+            <SearchBar />
+          </div>
+        </div>
+        <nav className={style.navButtons}>
+          {/* <details id="categories" className={style.details}>
       <summary>Categories</summary>
       {categories[0] &&
        categories.map((e, index) => {
@@ -66,54 +67,67 @@ export default function NavBar() {
         );
        })}
      </details> */}
-     <button onClick={showCategoriesHandler} className={style.categoriesButton}>
-      Categories
-     </button>
-     <div>
-      {categories[0] &&
-       showCategories &&
-       categories.map((e, index) => {
-        const { id, name } = e;
-        return (
-         <div key={index}>
-          <Link key={id} id={id} to={`/results`} onClick={onCategorySelection}>
-           {name}
+          <button
+            onClick={showCategoriesHandler}
+            className={style.categoriesButton}
+          >
+            Categories
+          </button>
+          <div>
+            {categories[0] &&
+              showCategories &&
+              categories.map((e, index) => {
+                const { id, name } = e;
+                return (
+                  <div key={index}>
+                    <Link
+                      key={id}
+                      id={id}
+                      to={`/results`}
+                      onClick={onCategorySelection}
+                    >
+                      {name}
+                    </Link>
+                  </div>
+                );
+              })}
+          </div>
+          <Link to="/" className={style.navBarLinks}>
+            History
           </Link>
-         </div>
-        );
-       })}
-     </div>
-     <Link to="/" className={style.navBarLinks}>
-      History
-     </Link>
-     <Link to="/" className={style.navBarLinks}>
-      Sales
-     </Link>
-     <Link to="/product/create" className={style.navBarLinks}>
-      Upload your product
-     </Link>
-    </nav>
-   </div>
+          <Link to="/" className={style.navBarLinks}>
+            Sales
+          </Link>
+          <Link to="/product/create" className={style.navBarLinks}>
+            Upload your product
+          </Link>
+        </nav>
+      </div>
 
-   <div className={style.sectionTwo}>
-    <div>
-     <Link to={""} className={style.logIn}>
-      Log in
-     </Link>
-    </div>
-    <div>
-     <Link to={"/signup"} className={style.signUp}>
-      Sign up
-     </Link>
-    </div>
-    <div>
-     <img src={cart} alt="cart"/>
-     {/* <p className={style.cartNumber}>{}</p> */}
-    </div>
-   </div>
-   <div>
-    <SignInGoogle />
-   </div>
-  </header>
- );
+      <div className={style.sectionTwo}>
+        <div>
+          <Link to={""} className={style.logIn}>
+            Log in
+          </Link>
+        </div>
+        <div>
+          <Link to={"/signup"} className={style.signUp}>
+            Sign up
+          </Link>
+        </div>
+        <div>
+          <Link to={"/shopping-cart"} className={style.shooping}>
+            <h4>
+              <img src={cartI} alt="cart" /> {cart.length}
+            </h4>
+          </Link>
+
+          {/* <p className={style.cartNumber}>{}</p> */}
+        </div>
+      </div>
+      <div>
+        <SignInGoogle />
+      </div>
+    </header>
+  );
 }
--- front/app/src/components/ShoppingCart/ShoppingCart.jsx
+++ front/app/src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,26 +1,74 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addToCart, clearCart, removeOneFromCart } from "../../redux/actions";
+import {
+  clearCart,
+  removeAllFromCart,
+  removeOneFromCart,
+  addOneFromCart,
+} from "../../redux/actions";
+import { Toaster, toast } from "react-hot-toast";
 
 export default function ShoppingCart() {
   const dispatch = useDispatch();
-  const cart = useSelector((state) => state.cart);
-  const [quantityAdd, setQuantityAdd] = useState(0);
-  const [quantityRes, setQuantityRes] = useState(0);
 
-  const addCart = (product, quantity) => {
-    dispatch(addToCart(product, quantity));
+  const cart = useSelector((state) => state.cart);
+
+  const [quantityAdd, setQuantityAdd] = useState(1);
+  const [quantityRes, setQuantityRes] = useState(1);
+
+  const addCart = (e) => {
+    dispatch(addOneFromCart(e.target.value));
+    toast.success(`Successfully added ${quantityAdd} items to the cart!`);
+  };
+
+  const resCart = (e) => {
+    dispatch(removeOneFromCart(e.target.value));
+    toast.success(`Successfully removed ${quantityRes} items to the cart!`, {
+      style: {
+        border: "1px solid #713200",
+        padding: "16px",
+        color: "#713200",
+      },
+      iconTheme: {
+        primary: "#713200",
+        secondary: "#FFFAEE",
+      },
+    });
   };
 
   const deleteFromCart = (e) => {
-    dispatch(removeOneFromCart(e.target.value));
+    dispatch(removeAllFromCart(e.target.value));
+    toast.success(`Successfully deleted product from the cart!`, {
+      style: {
+        border: "1px solid #FF2301",
+        padding: "16px",
+        color: "#FF2301",
+      },
+      iconTheme: {
+        primary: "#FF2301",
+        secondary: "#FFFAEE",
+      },
+    });
   };
 
   const deleteAll = () => {
-    dispatch(clearCart());
+    toast.success(`Successfully cleared cart!`, {
+      style: {
+        border: "1px solid  #F32013",
+        padding: "16px",
+        color: " #F32013",
+      },
+      iconTheme: {
+        primary: " #F32013",
+        secondary: "#FFFAEE",
+      },
+    });
+    setTimeout(() => {
+      dispatch(clearCart());
+    }, 1200);
   };
 
-  if (cart.length > 1)
+  if (cart.length > 0)
     return (
       <div>
         <h2>Shopping Cart</h2>
@@ -28,10 +76,11 @@ export default function ShoppingCart() {
           <h3>Products:</h3>
           {cart.map((pt) => {
             const images = pt.product.images.split(" ");
+
             return (
-              <div>
+              <div key={pt.product.id}>
                 <img src={images[0]} alt={pt.product.title} />
-                <h2>{pt.ptoduct.title}</h2>
+                <h2>{pt.product.title}</h2>
                 <h3>
                   <span>{pt.product.model}</span>
                   <span>{pt.product.brand}</span>
@@ -46,21 +95,27 @@ export default function ShoppingCart() {
                   </h2>
                 </div>
                 <div>
-                  <button onClick={() => addCart()}>
-                    +{pt.amount + quantityAdd}
+                  <button value={pt.product.id} onClick={(e) => addCart(e)}>
+                    +{quantityAdd}
                   </button>
-                  <button>-{pt.amount + quantityRes}</button>
-                  <button value={pt.product} onClick={(e) => deleteFromCart(e)}>
-                    X
+                  <button value={pt.product.id} onClick={(e) => resCart(e)}>
+                    -{quantityRes}
+                  </button>
+                  <button
+                    value={pt.product.id}
+                    onClick={(e) => deleteFromCart(e)}
+                  >
+                    -{pt.amount}
                   </button>
                 </div>
               </div>
             );
           })}
           <div>
-            <button onClick={() => clearCart()}>REMOVE ALL</button>
+            <button onClick={() => deleteAll()}>REMOVE ALL</button>
           </div>
         </div>
+        <Toaster />
       </div>
     );
   else return <div>Shooping cart is empty</div>;
