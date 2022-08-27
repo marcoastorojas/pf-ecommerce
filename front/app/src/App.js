398224e1d64f142b9bef2a7ce4b208bfbbb895db
Andres Torres <andrestorres9700@gmail.com>
2022-08-27 16:29:07 -0300

    general navbar css

--- front/app/src/App.js
+++ front/app/src/App.js
@@ -1,5 +1,3 @@
-// import logo from './logo.svg';
-import "./App.css";
 import React from "react";
 // import { useEffect } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
@@ -18,30 +16,28 @@ import SellerRoutes from "./validations/SellerRoutes";
 import NavBar from "./components/NavBar";
 import SimpleNavBar from "./components/SimpleNavBar";
 
-function App() {
- const { pathname } = useLocation();
+import "./App.css";
 
- return (
-  <div className="App">
-   {pathname === "/signup" || pathname === "/login" ? (
-    <SimpleNavBar />
-   ) : (
-    <NavBar />
-   )}
-   <Routes>
-    <Route path="/" exact element={<Landing />} />
-    <Route path="/signup" exact element={<SignUp />} />
-    <Route path="/login" exact element={<LogIn />} />
-    <Route path="/results" exact element={<Results />} />
-    <Route path="/product/:id" exact element={<Details />} />
-    <Route element={<SellerRoutes />}>
-     <Route path="/product/create" exact element={<Form />} />
-    </Route>
-    <Route path="/shopping-cart" exact element={<Cart />} />
-    <Route path="/checkout/" exact element={<CheckoutPay />} />
-   </Routes>
-  </div>
- );
+function App() {
+  const { pathname } = useLocation();
+
+  return (
+    <div className="App">
+      {pathname === "/signup" || pathname === "/login" ? <SimpleNavBar /> : <NavBar />}
+      <Routes>
+        <Route path="/" exact element={<Landing />} />
+        <Route path="/signup" exact element={<SignUp />} />
+        <Route path="/login" exact element={<LogIn />} />
+        <Route path="/results" exact element={<Results />} />
+        <Route path="/product/:id" exact element={<Details />} />
+        <Route element={<SellerRoutes />}>
+          <Route path="/product/create" exact element={<Form />} />
+        </Route>
+        <Route path="/shopping-cart" exact element={<Cart />} />
+        <Route path="/checkout/" exact element={<CheckoutPay />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
