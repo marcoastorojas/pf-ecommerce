a41754442efe41957da6366b92039c4eb4271cd0
ElRuidoyLaFuria <97857484+ElRuidoyLaFuria@users.noreply.github.com>
2022-08-30 16:36:01 -0300

    Update paymentRoutes.js

--- back/src/routes/paymentRoutes.js
+++ back/src/routes/paymentRoutes.js
@@ -5,70 +5,45 @@ const PaymentController = require("../controllers/paymentController")
 const PaymentService = require("../Services/PaymentService") 
 const PaymentInstance = new PaymentController(new PaymentService())
 
-const { Order, Orderdetail, Product } = require("../db")
+const { Order, Orderdetail, Product, User } = require("../db")
 //controllers
 const { postOrder } = require("../controllers/paymentModelController")
 
-// paymentRoutes.get('/', function(req, res, next){ 
+paymentRoutes.post("/", function (req, res, next) {
+  //Persiste el pedido en la b.d. y devuelve el link de pago.
+  let arr = [];
+  const idOrder = postOrder(req, res).then((idOrder) => {    
+    const order = Order.findByPk(idOrder, {
+      include: [{ model: Orderdetail, include: [Product] }],
+    })
+    .then((order) => {
+      const userName = User.findOne({ where: { uid: order.userId } });
+      userName.then((username) => {
+        const filteredOrder = {
+          orderId: order.id,
+          orderUser: {
+            idUser: order.userId,
+            userName: username,
+          },
+          orderDetail: order.orderdetails.map((product) => {
+            return {
+              idProduct: product.productId,
+              title: product.product.title,
+              quantity: product.quantity,
+              price: product.product.price,
+            };
+          }),
+        };
 
-//   Order.
-  
-
-// })
-
-paymentRoutes.post("/", function(req, res, next){   
-  //Persiste el pedido en la b.d. y devuelve el link de pago.  
-  let arr = []
-  const idOrder = postOrder(req, res) 
-  
-  .then((idOrder)=> { 
-     const order = Order.findByPk(idOrder,{
-      include: [{ model: Orderdetail, include: [Product] }],   
-     })      
-     
-     //.then(order => console.log('--> '+JSON.stringify(order)) )
-     .then(order => {
-      const filteredOrder = {
-        orderId: order.id,
-        orderDetail: [
-       {
-        product: {
-          productId: order.orderdetails.map(product=> product.productId),
-          title: order.orderdetails.title
-        }
-      }
-    
-    ]
-       }
-
-       console.log('--> '+JSON.stringify(filteredOrder))
-     }) 
-
-     
-    
-  })
-
-  })
-
-  //{
-    //   where: { id: idOrder }
-    //   //include: [{ model: Orderdetail, include: [Product] }],
-    // });
-  
-   // order.then((order)=> PaymentInstance.getPaymentLink(req,res).then((link)=> res.status(200).send([{link: link, order: order}])))   
-  // .then ((id)=>{    
-  // const order = Order.findByPk(id)  
-  // })
-  // .then((data)=>{console.log(data)})
- 
-
-//.then (() => PaymentInstance.getPaymentLink(req,res))      
-  
- 
-
+        link = PaymentInstance.getPaymentLink(req, res).then((link) =>{
+          console.log([{ link: link, order: JSON.stringify(filteredOrder) }])
+          res.status(200).send([{ link: link, order: JSON.stringify(filteredOrder) }])          
+        });
+      });
+    });
+  });
+});
 
 module.exports = {
-    paymentRoutes
-}
-
-
+  paymentRoutes,
+};
