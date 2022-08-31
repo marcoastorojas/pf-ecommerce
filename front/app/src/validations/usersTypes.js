9bd9d8c25534698fbfe69f01d6479b835ab0a4bc
asgonzales <asgonzalesromani@gmail.com>
2022-08-31 16:20:24 -0300

    css menUser

--- front/app/src/validations/usersTypes.js
+++ front/app/src/validations/usersTypes.js
@@ -1,3 +1,3 @@
-export const SELLER_ROLE = '4eaec57e-a9e7-4b32-920d-084381c7e7f7';
+export const SELLER_ROLE = 'ca047307-8f9e-4b84-adb2-5e3fe26d19c3';
 export const BUYER_ROLE = '653a9595-7480-4cb2-896f-bf4f3a7bc714'; //Also USER_ROLE
-export const ADMIN_ROLE = '90cde201-1098-44ea-b2bf-b430421eeda8';
+export const ADMIN_ROLE = '4abfb8c0-590d-453c-be4e-99ac0b21ced2';
