2d46493ff02a1e0eddc3a82157f4ee7db9900f24
asgonzales <asgonzalesromani@gmail.com>
2022-09-02 20:21:06 -0300

    usersCardsContainer omited in Admin Page

--- front/app/src/pages/AdminPage/AdminPage.jsx
+++ front/app/src/pages/AdminPage/AdminPage.jsx
@@ -14,7 +14,7 @@ export default function AdminPage () {
         <div>
             SOY EL ADMIN PAGE
 
-            <UsersCardsContainer/>
+            {/* <UsersCardsContainer/> */}
         </div>
         
     )
