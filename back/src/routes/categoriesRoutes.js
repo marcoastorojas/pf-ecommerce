3033ee02909c50d726a0817a057936c9fca21e8f
asgonzales <asgonzalesromani@gmail.com>
2022-09-03 14:36:08 -0300

    modify categories endpoints

--- back/src/routes/categoriesRoutes.js
+++ back/src/routes/categoriesRoutes.js
@@ -4,26 +4,50 @@ const categoriesroutes = Router()
 const { Op } = require("sequelize")
 
 categoriesroutes.post("/", async (req, res) => {
-    const { name } = req.body
+    const { name, active } = req.body
+    let categorie = {
+        name
+    }
+    if(!name) {
+        return res.status(400).json({ error: "falta nombre" })
+    }
+    if(!!active) {
+        categorie = {
+            ...categorie,
+            active
+        }
+    }
     const repetido = await Category.findOne({ where: { name } })
     if (repetido) return res.status(400).json({ error: "repetido" })
-    const nuevaCategoria = await Category.create({ name })
+    const nuevaCategoria = await Category.create(categorie)
     res.status(201).json(nuevaCategoria)
 })
 
 categoriesroutes.get("/", async (req, res) => {
-    const { name } = req.query
-    if (name) {
-        const categoria = await Category.findOne({ where: { name: { [Op.iLike]: name } } })
-        return res.status(200).json(categoria)
+    const { name, onlyActive } = req.query
+    // if (name) {
+    //     const categoria = await Category.findOne({ where: { name: { [Op.iLike]: name } } })
+    //     return res.status(200).json(categoria)
+    // }
+    // const lista = await Category.findAll({
+    //     include: [
+    //         { model: Subcategory, as: "subcategories" }, 
+    //         // { model: Product, as: "products",attributes: ['id','title', 'images','model','brand','subcategoryId'] }
+    //     ],
+    // })
+    let whereCond = {}
+    // console.log(onlyActive, !!onlyActive)
+    if (!!onlyActive) {
+        whereCond = {
+            active: onlyActive.toString()
+        }
     }
-    const lista = await Category.findAll({
-        include: [
-            { model: Subcategory, as: "subcategories" }, 
-            // { model: Product, as: "products",attributes: ['id','title', 'images','model','brand','subcategoryId'] }
-        ],
+    const categories = await Category.findAll({
+        where: whereCond
     })
-    res.status(200).json({ data: lista })
+    // console.log(whereCond)
+    // res.status(200).json({ data: lista })
+    res.status(200).json({data: categories})
 })
 
 module.exports = { categoriesroutes }
