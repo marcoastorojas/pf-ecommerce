e54b7331263f5d0634957763676ae977e83d7bae
alejandrogh7 <alejo1garciasosa@gmail.com>
2022-09-05 20:00:39 -0500

    map ready

--- front/app/src/components/Checkout/Checkout.jsx
+++ front/app/src/components/Checkout/Checkout.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { Toaster, toast } from "react-hot-toast";
-import { sendPayment } from "../../redux/actions";
+import { sendPayment, getSucursal } from "../../redux/actions";
 import { useState } from "react";
 import Mapita from "../Mapita/Mapita.jsx"
 import "./Checkout.css"
@@ -14,6 +14,7 @@ export default function Checkout() {
   const cart = useSelector((state) => state.cart);
   const cartTotal = useSelector((state) => state.cartTotal);
   const user = useSelector((state) => state.user);
+  
   //RECIBE LO QUE EL POST DEL BACK RETORNA
   const dataPayment = useSelector((state) => state.dataPayment);
   const navigate = useNavigate();
@@ -68,6 +69,7 @@ export default function Checkout() {
             error,
         });
     };
+    
      useEffect(()=>{
         if(!("geolocation" in navigator)) {
             onError({
@@ -78,7 +80,7 @@ export default function Checkout() {
         navigator.geolocation.getCurrentPosition(onSuccess,onError);
     }, []);
 
-  if (cart.length < 1) {
+  if (cart.length < 0) {
     toast.error("Shopping cart is empty");
     toast.custom(
       (t) => (
