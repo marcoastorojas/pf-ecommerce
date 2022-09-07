503707922325d44cd71d6ff498facb63076ecb86
asgonzales <asgonzalesromani@gmail.com>
2022-09-07 20:34:54 -0300

    imageAdmin pt1

--- front/app/src/components/SellerDetails/index.jsx
+++ front/app/src/components/SellerDetails/index.jsx
@@ -1,11 +1,18 @@
 import sampleImage1 from "../../media/images/sample-image-square.jpg";
 import style from './index.module.css';
 import emptyUser from "../../media/images/empty_user_profilepic.png";
+import { ADMIN_ROLE } from "../../validations/usersTypes";
 
 export default function SellerDetails({seller}) {
+  let imagen = emptyUser
+  if(seller.uid === ADMIN_ROLE) {
+    
+  }
+  
   return (
     <div className={style.contSeller}>
-      <img src={seller.image!==null?seller.image:emptyUser} alt="user profile" />
+      <button onClick={() => console.log(seller)}>PRUEBA</button>
+      <img src={seller.image!==null?seller.image:imagen} alt="user profile" />
       <h2>{seller.username}</h2>
       <div className="user-data">
         {/* <p>-Seller Popularity-</p> */}
