2bb7f5e574ec4adf341fb1c7a439d008f6bae56b
asgonzales <asgonzalesromani@gmail.com>
2022-09-07 20:41:36 -0300

    image admin in product detail

--- front/app/src/components/SellerDetails/index.jsx
+++ front/app/src/components/SellerDetails/index.jsx
@@ -1,18 +1,21 @@
 import sampleImage1 from "../../media/images/sample-image-square.jpg";
 import style from './index.module.css';
 import emptyUser from "../../media/images/empty_user_profilepic.png";
+import adminImage from '../../media/images/plushardware.png';
 import { ADMIN_ROLE } from "../../validations/usersTypes";
+import { useState } from "react";
 
 export default function SellerDetails({seller}) {
-  let imagen = emptyUser
-  if(seller.uid === ADMIN_ROLE) {
-    
-  }
+  const  [imagenAdmin, setImagenAdmin ] = useState(seller.roleId===ADMIN_ROLE?adminImage:emptyUser)
+  // setImagenAdmin(emptyUser)
+  // if(seller.uid === ADMIN_ROLE) {
+  //   setImagenAdmin(adminImage)
+  // }
   
   return (
     <div className={style.contSeller}>
       <button onClick={() => console.log(seller)}>PRUEBA</button>
-      <img src={seller.image!==null?seller.image:imagen} alt="user profile" />
+      <img src={seller.image!==null?seller.image:imagenAdmin} alt="user profile" referrerPolicy="no-referrer"/>
       <h2>{seller.username}</h2>
       <div className="user-data">
         {/* <p>-Seller Popularity-</p> */}
