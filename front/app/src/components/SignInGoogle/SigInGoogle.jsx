5978ae6a847528e91cf54461d6511488e37a406a
marco <you@example.com>
2022-11-14 01:19:41 -0500

    change login google method

--- front/app/src/components/SignInGoogle/SigInGoogle.jsx
+++ front/app/src/components/SignInGoogle/SigInGoogle.jsx
@@ -5,39 +5,44 @@ import { useDispatch, useSelector } from 'react-redux';
 import jwt_decode from 'jwt-decode';
 import { setUserGoogle } from '../../redux/actions';
 import { Navigate } from 'react-router-dom';
-// import { toast } from 'react-hot-toast';
+import { gapi } from 'gapi-script';
+import GoogleLogin from 'react-google-login';
 
-export default function SignInGoogle () {
+
+export default function SignInGoogle() {
     const dispatch = useDispatch();
     const user = useSelector(state => state.user)
-    function handleCallbackResponse (response) {
-        dispatch(setUserGoogle(response.credential))
-        // localStorage.setItem('user', JSON.stringify(jwt_decode(response.credential)))
+    const clientID = "417222371026-r77ea9el6hem23ioc9u51284pe3aq5u1.apps.googleusercontent.com"
+    function handleCallbackResponse(response) {
+        dispatch(setUserGoogle(response.tokenId))
+        localStorage.setItem('user', JSON.stringify(jwt_decode(response.tokenId)))
         document.getElementById('sigInDiv').hidden = true
     }
     //pruebaDeploy
     useEffect(() => {
-        window.google && window.google.accounts.id.initialize( {
-            client_id: '98217127170-vfi6dqi6v82dhj9704ra9gb2go8n6q7l.apps.googleusercontent.com',
-            callback: handleCallbackResponse
-        });
-        window.google && Object.keys(user).length === 0 && window.google.accounts.id.renderButton(
-            document.getElementById('sigInDiv'),
-            { theme: 'outline', size: 'large' }
-        );
-        window.google && Object.keys(user).length === 0 && window.google.accounts.id.prompt()
+        const start = () => {
+            gapi.auth2.init({
+                clientId: clientID
+            })
+        }
+        gapi.load("client:auth2", start)
+
     }, [])
     const handleSignOut = () => {
         // setUser({})
         dispatch(setUserGoogle({}))
-        document.getElementById('sigInDiv').hidden = false
+        // document.getElementById('sigInDiv').hidden = false
     }
     return (
-        <div className={style.contsigin}>
+        <div className={style.contsigin} id="sigInDiv">
             <div>
                 <h5>Or continue with</h5>
             </div>
-            <div id='sigInDiv'></div>
+            <GoogleLogin
+                clientId={clientID}
+                onSuccess={handleCallbackResponse}
+                onFailure={(data) => { console.log({ error: data }) }}
+            />
         </div>
     )
 }
